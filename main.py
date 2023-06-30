9f68c43b4eebd1f0cbe8c7f4fc26c1c2c9fafd69
Daniel Ivánek <ivanek.daniel99@gmail.com>
2023-06-30 14:43:19 +0200

    commit

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-print("Hello world")
+"""
+projekt_1.py: 
+author: Daniel Ivánek   
+email: ivanek.daniel99@gmail.com
+discord: notme1275
+"""
+import task_template
+print(task_template.TEXTS)
+
+Registered_accounts = {"bob" : "123" , "ann" : "pass123" , 
+                       "mike" : "password123", "liz" : "pass123"}
+
+# Vyzadat si prihlasovaci jmeno a heslo
+# Zadat jmeno a heslo
+#  IF jmeno se shoduje s heslem:
+        # ----------------------------------------
+        # Welcome to the app, bob
+        # We have 3 texts to be analyzed.
+        # ----------------------------------------
+# Else:
+        # unregistered user, terminating the program..
+# Vyber text od 1 do 3
+#  IF text neni v seznamu textu:
+        # Sdel mu to a ukonci program(Nahradit smyckou for)
+#  ELIF zada neco jineho nez cislo ukonci program(nahradit smyckou for)
+# Pro zvoleny text vypocitej -  
+                            # počet slov,
+                            # počet slov začínajících velkým písmenem,
+                            # počet slov psaných velkými písmeny,
+                            # počet slov psaných malými písmeny,
+                            # počet čísel (ne cifer),
+                            # sumu všech čísel (ne cifer) v textu.
+# Vytiskni vysledek
+
+
+
+
+
